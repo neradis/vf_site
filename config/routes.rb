6c9f45d32fcd53814729a1766f6fae87eeedf781
neradis <neradis@fusionfactory.de>
2014-02-12 14:55:06 +0100

    Revert "added page-images-engine, added decoration of img-tags in page content with class-attribute values for YAML-alignment"
    This reverts commit 6960bdc8e6da0ca3f78ba8d0a937caee64bb39c6.

--- config/routes.rb
+++ config/routes.rb
@@ -59,4 +59,8 @@ VfRefinery::Application.routes.draw do
   # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
+
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  match 'debug/:controller(/:action(/:id))(.:format)'
 end
